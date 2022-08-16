64b2a61a8c1aee6f801a4f91cb75161305c04974
Raiyan <tajkierhaque@gmail.com>
2022-08-16 14:49:06 +0600

    assignment Given

---
+++ Module-1 (Redux-fundamentals)/Assignment/Muticounter-app/html_template/script.js
@@ -0,0 +1,24 @@
+const counterContainerEl = document.getElementById("counters-container");
+const addCounterEl = document.getElementById("add-counter");
+const resetBtnEl = document.getElementById("reset-btn");
+
+const initialState = {
+  counters: [
+    {
+      id: 1,
+      value: 0,
+    },
+  ],
+};
+
+// action identifires
+
+const counterReducer = (state, { type, payload }) => {
+  switch (type) {
+    case value:
+      break;
+
+    default:
+      break;
+  }
+};
